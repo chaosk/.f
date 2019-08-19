2a5eabb6ace6924faa7e1a68466dcde561d9a259
Krzysztof Socha <ksocha@ksocha.com>
2019-08-20 01:26:08 +0200

    🧨

--- .f/install.sh
+++ .f/install.sh
@@ -3,3 +3,8 @@
 mkdir -p $HOME/.f
 
 git clone --bare git@github.com:chaosk/.f.git $HOME/.f/.git
+
+alias dotf='/usr/bin/git --git-dir=$HOME/.f/.git/ --work-tree=$HOME'
+
+dotf checkout
+dotf config --local status.showUntrackedFiles no
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # .f
+
+
+Modelled on [The best way to store your dotfiles: A bare Git repository](https://www.atlassian.com/git/tutorials/dotfiles).

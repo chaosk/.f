315692785ab5231d145f07d628b3de93b895b728
Krzysztof Socha <ksocha@ksocha.com>
2019-08-20 01:53:56 +0200

    🧨

--- .f/brew/install.sh
+++ .f/brew/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Hombrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 xargs brew install < packages.txt
 xargs brew cask install < casks.txt
